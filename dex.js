3967943d2b652d9f22827c8ed7afc94f4c1d1649
FERNANDEZATZIRY <fernandezandrea4cp@gmail.com>
2023-04-27 16:08:12 +0000

    actividad

--- dex.js
+++ dex.js
@@ -1,1 +1,60 @@
-console.log("fg")
+import http from "http";
+const server = http.createServer((req, res) => {
+  // Desestructurando de "req"
+  let { url, method } = req;
+  console.log(`📣 CLIENT-REQUEST: ${req.url} ${req.method}`);
+
+  // Enrutando peticiones
+  switch (url) {
+    case '/':
+      // Peticion raiz
+      // Estableciendo cabeceras
+      res.setHeader('Content-Type', 'text/html');
+      // Escribiendo la respuesta
+      res.write(`
+      <html>
+        <head>
+          <link rel="icon" type="image/png" sizes="32x32" href="https://img.icons8.com/fluency/256/domain.png">
+          <title>My App</title>
+        </head>
+        <body> 
+          <h1 style="color: #333">Hello from my server</h1>
+          <p style="color: #34495E">Estas en el recurso raiz.</p>
+        </body>
+      </html>
+      `);
+      console.log(`📣 Respondiendo: 200 ${req.url} ${req.method}`);
+      // Estableciendo codigo de respuesta
+      res.statusCode = 200;
+      // Cerrando la comunicacion
+      res.end();
+      break;
+    default:
+      // Peticion raiz
+      // Estableciendo cabeceras
+      res.setHeader('Content-Type', 'text/html');
+      // Escribiendo la respuesta
+      res.write(`
+      <html>
+        <head>
+          <link rel="icon" type="image/png" sizes="32x32" href="https://img.icons8.com/fluency/256/domain.png">
+          <title>My App</title>
+        </head>
+        <body> 
+          <h1>&#128534; 404 Recurso no encontrado</h1>
+          <p>Lo sentimos pero no tenemos lo que buscas...</p>
+        </body>
+      </html>
+      `);
+      console.log(`📣 Respondiendo: 404 ${req.url} ${req.method}`);
+      // Estableciendo codigo de respuesta
+      res.statusCode = 404;
+      // Cerrando la comunicacion
+      res.end();
+      break;
+  }
+}); 
+
+server.listen(3000, "0.0.0.0", () => {
+  console.log("👩‍🍳 Servidor escuchando en http://localhost:3000"); 
+});
